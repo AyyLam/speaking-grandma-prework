de2c1c01a7d8ab5ab0b3b3431b9f774369734440
Anthony Lam <al3101@nyu.edu>
2018-05-05 03:18:36 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,12 +14,10 @@
 
 
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!":
+  if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-    elsif phrase.upcase == phrase
-    phrase.upcase == phrase
+  elsif phrase.upcase == phrase
+    return "NO, NOT SINCE 1938" 
+  else 
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase.
-    
-  
 end 
