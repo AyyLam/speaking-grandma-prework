cdfe08dbae4ef6d693ab803e2f55694c5dd6d0be
Anthony Lam <al3101@nyu.edu>
2018-05-05 03:21:46 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -19,5 +19,6 @@ def speak_to_grandma(phrase)
   elsif phrase.upcase == phrase
     return "NO, NOT SINCE 1938" 
   else 
-    return "HUH?! SPEAK UP, SONNY!"
-  end
+    return 'HUH?! SPEAK UP, SONNY!'
+end
+  
